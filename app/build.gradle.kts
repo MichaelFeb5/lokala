1eb9d8cfecc2de6c07428261d3f8752ca90efd78
michael <2111016210012@mhs.ulm.ac.id>
2023-12-10 16:00:21 +0800

    Feat<CRUD> : Adding List Layout For Read Orang Hilang

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -81,4 +83,10 @@ dependencies {
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.14.2")
+
+    // Shadow Card
+    //noinspection GradleCompatible
+    implementation ("com.android.support:cardview-v7:21.+")
+    //noinspection GradleCompatible
+    implementation ("com.android.support:recyclerview-v7:21.+")
 }
